6b34fe035fc9c2a7ccc109be1b0b53ae9bd8c569
kevin-kavout <31523066+Kevinqian0501@users.noreply.github.com>
2018-02-26 15:29:04 -0800

    Update setup_cuda9_cuDNNv7.sh

--- setup_cuda9_cuDNNv7.sh
+++ setup_cuda9_cuDNNv7.sh
@@ -10,7 +10,7 @@ wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-re
 sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
 sudo apt-get update
-sudo apt-get install cuda
+sudo apt-get --assume-yes install cuda
 
 echo "export PATH=/usr/local/cuda-9.0/bin\${PATH:+:\${PATH}}" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64\${LD_LIBRARY_PATH:+:\${LD_LIBRARY_PATH}}" >> ~/.bashrc
