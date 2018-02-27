30ebbbeb43acdd3e312ec8aa84c6472a2868bb63
kevin-kavout <31523066+Kevinqian0501@users.noreply.github.com>
2018-02-26 16:00:11 -0800

    Update setup_cuda9_cuDNNv7.sh

--- setup_cuda9_cuDNNv7.sh
+++ setup_cuda9_cuDNNv7.sh
@@ -28,8 +28,11 @@ sudo cp lib64/* /usr/local/cuda/lib64/
 sudo cp include/* /usr/local/cuda/include/
 
 #install pip3
-sudo apt-get --assume-yes install python3-pip python3-dev
+sudo apt-get --assume-yes install python3-pip python3-dev python-virtualenv
 pip3 install --upgrade pip
+virtualenv --system-site-packages -p python3 project
+source ~/project/bin/activate
+pip3 install tensorflow-gpu==1.5.0
 
 #install jupyter
 pip3 install jupyter
